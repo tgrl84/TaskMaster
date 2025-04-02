62a5591a5510266b8b75dda5162cd67604c55b13
Tristan Guillemier <71886156+tgrl84@users.noreply.github.com>
2025-04-02 10:46:35 +0200

    Init maui

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskMaster;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TaskMaster;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
